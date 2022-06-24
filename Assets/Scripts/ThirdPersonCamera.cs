6a399e6defc8952d41c4e7723eed97f65f359b39
doubledeltas <thesmartist@naver.com>
2022-06-24 23:17:20 +0900

    Update: Player Rotation and Camera Moving

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -7,15 +7,21 @@ public class ThirdPersonCamera : MonoBehaviour
     public GameObject target;
     public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
+    float trueRotation;
+
+    private void Awake()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.transform.position + offset;
+        float trueRotation = target.GetComponent<PlayerController>().mouseRotation;
+
+        float angle = Mathf.Deg2Rad * trueRotation;
+        Vector3 relZUnit = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+
+        transform.position = target.transform.position + offset.y * Vector3.up + relZUnit * offset.z;
+        transform.eulerAngles = new Vector3(0, trueRotation, 0);
     }
 }
