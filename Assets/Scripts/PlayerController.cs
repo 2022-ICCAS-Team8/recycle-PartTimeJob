63ff3f86cdd83fbbc32cc79a6a3c1dc5b2676c86
choheejin <hjcho9510@gmail.com>
2022-06-25 00:08:19 +0900

    fix: conflict error

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,46 +6,69 @@ public class PlayerController: MonoBehaviour
 {
     public float speed;
     public Vector2 rotationSensitivity;
+    public float rotationVelocity;
 
     float xInput, zInput;
     float xMouse;
 
-    float rotationOffset;
+    public float mouseRotation;
+    float wasdRotation;
+    float internalRotation;
 
     Animator anim;
+
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        mouseRotation = transform.eulerAngles.y;
+        wasdRotation = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         xMouse = Input.GetAxis("Mouse X");
-        float rotateY = xMouse * rotationSensitivity.x;
-
         xInput = -Input.GetAxisRaw("Horizontal");
-        zInput = Input.GetAxisRaw("Vertical");
-
+        zInput = +Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(xInput, 0, zInput).normalized;
-        transform.Rotate(Vector3.up, rotateY);
 
-        rotationOffset = Vector3.Angle(Vector3.forward, direction);
-        if (direction.x > 0)
-            rotationOffset *= -1.0f;
+        if (direction != Vector3.zero)
+        {
+            wasdRotation = Vector3.Angle(Vector3.forward, direction);
+            if (direction.x > 0)
+                wasdRotation *= -1.0f;
+        }
+        mouseRotation += xMouse * rotationSensitivity.x;
+        internalRotation = mouseRotation + wasdRotation;
+        internalRotation -= 360.0f * Mathf.Floor((internalRotation + 180.0f) / 360.0f);
 
-        Debug.Log(rotationOffset);
+        float displayDiff = internalRotation - transform.eulerAngles.y;
+        if (displayDiff > 180.0f)
+            displayDiff -= 360.0f;
+        else if (displayDiff < -180.0f)
+            displayDiff += 360.0f;
 
-        float totRotationInRadian = Mathf.Deg2Rad * (transform.eulerAngles.y + rotationOffset);
+        float displayRotation = transform.eulerAngles.y;
+
+        float dth = rotationVelocity * Time.deltaTime;
+        if (displayDiff > dth)
+            displayRotation += dth;
+        else if (displayDiff < -dth)
+            displayRotation -= dth;
+        else
+            displayRotation = internalRotation;
+
+        transform.eulerAngles = new Vector3(0, displayRotation, 0);
+
+        // Debug.Log("DR: " + displayRotation + " / IR: " + internalRotation);
+
+        float internalRotationInRad = Mathf.Deg2Rad * internalRotation;
         transform.localPosition +=
-            new Vector3(Mathf.Sin(totRotationInRadian), 0, Mathf.Cos(totRotationInRadian))
+            new Vector3(Mathf.Sin(internalRotationInRad), 0, Mathf.Cos(internalRotationInRad))
             * direction.magnitude * Time.deltaTime * speed;
 
         anim.SetBool("isWalking", direction != Vector3.zero);
-
-
     }
 }
